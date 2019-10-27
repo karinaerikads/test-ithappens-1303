f6d8fb8d45a5b5b4905929cb67d416a788846542
karinaerikads <karinaerikads@hotmail.com>
2019-10-27 03:37:08 -0200

    dependências do spring boot e modelo parallax-template do materializecss adicionados ao projeto

---
+++ implementacao/test1303ithappens/src/main/java/com/ithappenstest1303/test1303ithappens/Test1303ithappensApplication.java
@@ -0,0 +1,13 @@
+package com.ithappenstest1303.test1303ithappens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test1303ithappensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test1303ithappensApplication.class, args);
+	}
+
+}
